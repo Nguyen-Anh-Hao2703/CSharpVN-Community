3ade21a4573dd790fd2eb25c37a5cb76312c13ba
Anh Hào Nguyễn <anhhaonguyen27032013@outlook.com>
2026-03-09 11:12:18 +0700

    Them trang xem dong gop

--- CSharpVN Community/Pages/Home.cshtml.cs
+++ CSharpVN Community/Pages/Home.cshtml.cs
@@ -5,8 +5,25 @@ namespace CSharpVN_Community.Pages
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGetXemDongGop(string key)
         {
+            string secretKey = "@nhhao27O3"; // Mật khẩu của Hào
+
+            if (key != secretKey)
+            {
+                return Content("Bạn không có quyền truy cập!");
+            }
+
+            // Đường dẫn này sẽ lấy file ngay tại thư mục gốc của ứng dụng trên Render
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "DongGopCode.txt");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return Content("Chưa có ai đóng góp code cả.");
+            }
+
+            string content = System.IO.File.ReadAllText(filePath);
+            return Content(content);
         }
     }
 }
