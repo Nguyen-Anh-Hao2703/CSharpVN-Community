84b3c36ef1f2b52216c6491744d74542986152ea
Anh Hào Nguyễn <anhhaonguyen27032013@outlook.com>
2026-03-10 08:36:15 +0700

    Add Dockerfile

---
+++ CSharpVN Community/Pages/Download_CSharpVN.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharpVN_Community.Pages
+{
+    public class Download_CSharpVNModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
